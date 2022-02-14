aa9788d397ea69c7c88328add2cb4060a9ee891c
Subin Fernandez <Subin.Fernandez@gmail.com>
2022-02-14 21:43:33 +0000

    Fixed accessibility issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head />
         <body>
           {/* eslint-disable @next/next/no-sync-scripts */}
